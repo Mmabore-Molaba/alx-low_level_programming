56b6088ff79afea37c12eae419b0f2fea1510f82
Mmabore-Molaba <mbree92@gmail.com>
2022-07-05 13:55:37 -0700

    Script 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,6 +1,7 @@
 #include "main.h"                                                                                                                              
 /**
-*	main - Print _putchar                                               *
+*	main - Print _putchar
+*
 *	Return: Always 0(success)
 */
 int main(void)
