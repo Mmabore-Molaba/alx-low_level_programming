ba5ad80262d1a45e3c4b3eb28eac6bbaa8ca048e
Mmabore-Molaba <mbree92@gmail.com>
2022-07-06 12:52:52 -0700

    Script 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,10 +15,11 @@ void jack_bauer(void)
 
 			_putchar((H / 10) + '0');
 			_putchar((H % 10) + '0');
-			_putchar((':');
+			_putchar(':');
 			_putchar((M / 10) + '0');
 			_putchar((M % 10) + '0');
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
+
 }
