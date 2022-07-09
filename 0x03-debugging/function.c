937947bcad9cee35c77b3f8419296df02bf05774
Mmabore-Molaba <mbree92@gmail.com>
2022-07-08 17:12:08 -0700

    Script 0

---
+++ 0x03-debugging/function.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void positive_or_negative(int i)
+{
+
+	int n;                                                                    
+	srand(time(0));
+	n = i;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0); 
+/**
+*	main - debugging  task
+*	Return: 0
+*/
+int main(void)                                                               
+	{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+	}
+}
+
