53db16a001fd872d58343e7706ee119c4ba5a73f
Mmabore-Molaba <mbree92@gmail.com>
2022-07-12 14:28:51 -0700

    Script 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,8 +1,18 @@
 #include "main.h"
 /**
-*_puts - Write a function that prints a string, followed by a new line, to stdout
+*	_puts - function prints a string, followed by a new line, to stdout
+*	@str: the string
 *
-*Return: Always 0.
+*	Return: the length of the string
 */
-void _puts(char *str);
+void _puts(char *str)
 {
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
