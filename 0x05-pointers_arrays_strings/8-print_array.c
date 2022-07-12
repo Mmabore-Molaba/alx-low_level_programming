c520f59a171f1e5e6fa1b8aa9bcbb0f0106a4c1c
Mmabore-Molaba <mbree92@gmail.com>
2022-07-12 15:42:55 -0700

    Script 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
 *	print_array - prints n times the elements of an array
 *	@a: the pointer to the elements
@@ -10,11 +11,11 @@ void print_array(int *a, int n)
 
 	for (i = 0 ; i < n ; i++)
 	{
-		printtf("%d", a[i]);
+		printf("%d", a[i]);
 
 		if (i < n - 1)
 		{
-			printf(", ");
+			printf(",");
 		}
 	}
 	putchar('\n');
