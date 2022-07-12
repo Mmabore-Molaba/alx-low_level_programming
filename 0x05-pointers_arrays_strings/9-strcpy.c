c385714a88e18cff7e48c8b5a30699370a96bc03
Mmabore-Molaba <mbree92@gmail.com>
2022-07-12 15:56:54 -0700

    Script 9

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,33 @@
-#include <main.h>
+#include "main.h"
 /**
 *	*_strcpy - copies the string pointed to by src to the buffer pointed to by dest
+*	@dest: a buffur to copy the string to
+*	@src: the source of string to copy
 *
-*
+*	Return: a pointer to the destination string @dest
 */
 char *_strcpy(char *dest, char *src)
 {
+	int n = 0;
+	int i;
 
+	while (src[n] != '\0')
+	{
+		n++;
+	}
+
+	i = 0;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while (i <= n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
 }
