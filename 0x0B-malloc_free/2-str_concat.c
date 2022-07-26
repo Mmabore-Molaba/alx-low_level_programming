5cd37a6062206615767d508ee5d0f82730336563
Mmabore-Molaba <mbree92@gmail.com>
2022-07-25 20:54:06 -0700

    Script 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 	while (s1[i])
 		i++;
 	while (s2[j])
@@ -23,12 +23,14 @@ char *str_concat(char *s1, char *s2)
 
 	l = i + j;
 	s = (char *)malloc(l * sizeof(char) + 1);
+	if ( s == NULL)
+		return (NULL);
 	j = 0;
 	while (k < l)
 	{
-		if (k <= i)
+		if (k < i)
 			s[k] = s1[k];
-		if (k > i)
+		if (k >= i)
 		{
 			s[k] = s2[j];
 			j++;
